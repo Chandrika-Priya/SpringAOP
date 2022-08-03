6c890c01ff3e228d7281b7e6bcf40a429dd48736
Chandrika-Priya <chandrika.bogadi@gmail.com>
2022-08-03 16:36:19 +0530

    ADD. Spring AOP point cut expression of type execution executing for a method that matches the class name.

--- src/main/java/com/practice/spring/aspect/LoggingAspect.java
+++ src/main/java/com/practice/spring/aspect/LoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.practice.spring.dao.AccountDao.addAccount())")
     public void beforeAccount(){
         System.out.println("executing before add account");
     }
