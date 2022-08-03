b6c33d4d494c0d07839188299f36ea218b6e4073
Chandrika-Priya <chandrika.bogadi@gmail.com>
2022-08-03 16:34:23 +0530

    ADD. Spring AOP point cut expression of type execution executing for any method that matches name.

--- src/main/java/com/practice/spring/SpringAopApplication.java
+++ src/main/java/com/practice/spring/SpringAopApplication.java
@@ -1,6 +1,7 @@
 package com.practice.spring;
 
 import com.practice.spring.dao.AccountDao;
+import com.practice.spring.dao.MembershipDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,7 +12,9 @@ public class SpringAopApplication {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(springAOPconfig.class);
 		AccountDao accountDao = applicationContext.getBean("accountDao",AccountDao.class);
+		MembershipDao membershipDao = applicationContext.getBean("membershipDao",MembershipDao.class);
 		accountDao.addAccount();
+		membershipDao.addAccount();
 		applicationContext.close();
 //		SpringApplication.run(SpringAopApplication.class, args);
 	}
--- src/main/java/com/practice/spring/dao/MembershipDao.java
+++ src/main/java/com/practice/spring/dao/MembershipDao.java
@@ -3,7 +3,7 @@ package com.practice.spring.dao;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AccountDao {
+public class MembershipDao {
     public void addAccount(){
         System.out.println(getClass()+" : adding account to db");
     }
